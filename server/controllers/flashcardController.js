09976587700ec33f98c52436eb006eab0101242a
addymwenda12 <muneneaddy@gmail.com>
2024-08-13 17:15:12 +0300

    feat: Add getFlashcardSets controller function

---
+++ server/controllers/flashcardController.js
@@ -0,0 +1,18 @@
+import flashcardService from '../services/flashcardService';
+
+async function getFlashcardSets(req, res) {
+  try {
+    const userId = req.query.userId;
+    if (!userId) {
+      return res.status(400).json({ error: "Missing userId" });
+    }
+    const flashcardSets = await flashcardService.getFlashcardsSets(userId);
+  } catch (error) {
+    console.error("Error getting flashcards sets", error);
+    res.status(500).json({ error: "Failed to get flashcards sets" });
+  }
+}
+
+export default {
+  getFlashcardSets,
+};
