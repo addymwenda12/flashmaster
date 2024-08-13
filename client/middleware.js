028b623abfdf2673e988a96f2aedfbb7aea1be77
addymwenda12 <muneneaddy@gmail.com>
2024-08-13 11:41:50 +0300

    Added middleware for clerk authentication

---
+++ client/middleware.js
@@ -0,0 +1,12 @@
+const { clerkMiddleware } = require("@clerk/nextjs/server");
+
+module.exports = clerkMiddleware();
+
+module.exports.config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
