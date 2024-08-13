87a0042a5e717222ebebe10ce6919ee8132549fa
addymwenda12 <muneneaddy@gmail.com>
2024-08-13 17:20:53 +0300

    refactor: Add getStatistics controller function

---
+++ server/controllers/statisticsController.js
@@ -0,0 +1,19 @@
+import statisticsService from '../services/statisticsService';
+
+async function getStatistics(req, res) {
+  try {
+    const userId = req.query.userId;
+    if (!userId) {
+      return res.status(400).json({ error: "Missing userId" });
+    }
+    const statistics = await statisticsService.getStatistics(userId);
+    res.status(200).json(statistics);
+  } catch (error) {
+    console.error("Error getting statistics: ", error);
+    res.status(500).json({ error: "Failed to get statistics" });
+  }
+}
+
+export default {
+  getStatistics,
+};
