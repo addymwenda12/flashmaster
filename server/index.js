810ead46eb80322d3cb90c9d79295589695a011b
addymwenda12 <muneneaddy@gmail.com>
2024-08-13 15:52:23 +0300

    Added configurtion in the main file

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const flashcardRoutes = require("./routes/flashcard.js");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+/* ROUTES */
+app.use("/api/flashcards", flashcardRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
