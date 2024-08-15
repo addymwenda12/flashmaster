850b9a9f1ab71f3885146c4941be5044181e8dcc
addymwenda12 <muneneaddy@gmail.com>
2024-08-15 18:15:05 +0300

    Added new endpoint to get daily study streaks

--- server/controllers/progressController.js
+++ server/controllers/progressController.js
@@ -14,6 +14,21 @@ async function getStudyProgress(req, res) {
   }
 }
 
+async function getDailyStudyStreak(req, res) {
+  try {
+    const userId = req.query.userId;
+    if (!userId) {
+      return res.status(400).json({ error: "Missing userId" });
+    }
+    const streaks = await progressService.getDailyStudyStreak(userId);
+    res.status(200).json(streaks);
+  } catch (error) {
+    console.error("Error getting daily study streaks: ", error);
+    res.status(500).json({ error: "Failed to get daily study streaks" });
+  }
+}
+
 export default {
   getStudyProgress,
+  getDailyStudyStreak,
 };
