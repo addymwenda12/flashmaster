b47747d79893bedae85b015679e07204ea6ed6d8
addymwenda12 <muneneaddy@gmail.com>
2024-08-15 20:14:01 +0300

    chore: Add friend routes and controller for managing friend requests and interactions

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import flashcardRoutes from "./routes/flashcard.js";
 import vertexRoutes from "./routes/vertex.js";
 import languageRoutes from "./routes/language.js";
 import subscriptionRoutes from "./routes/subscription.js";
+import friendRoutes from "./routes/friend.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use("/api/flashcards", flashcardRoutes);
 app.use("/api/vertex", vertexRoutes);
 app.use("/api/languages", languageRoutes);
 app.use("/api/subscription", subscriptionRoutes);
+app.use("/api/friend", friendRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
