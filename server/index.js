69b979c3d827dfbf93098c0f20946f523272fb8f
addymwenda12 <muneneaddy@gmail.com>
2024-08-15 17:32:13 +0300

    feat: Add subscription routes and controller

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import flashcardRoutes from "./routes/flashcard.js";
 import vertexRoutes from "./routes/vertex.js";
 import languageRoutes from "./routes/language.js";
+import subscriptionRoutes from "./routes/subscription.js";
+
 dotenv.config();
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use("/api/flashcards", flashcardRoutes);
 app.use("/api/vertex", vertexRoutes);
 app.use("/api/languages", languageRoutes);
+app.use("/api/subscription", subscriptionRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
