a0b54461566be4b986a9a19ecc363fed13bbfc63
addymwenda12 <muneneaddy@gmail.com>
2024-08-15 18:20:31 +0300

    Fixed importation of firebase to subscriptionService

--- server/services/subscriptionService.js
+++ server/services/subscriptionService.js
@@ -1,5 +1,5 @@
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db } from '../firebase.js';
 import stripe from '../config/stripe.js';
 
 /* GET SUBSCRIPTION TIER */
