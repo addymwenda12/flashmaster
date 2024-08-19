482e1d3256534a1692e153caf4dd09ab4e1b522a
addymwenda12 <muneneaddy@gmail.com>
2024-08-19 20:12:32 +0300

    chore: Add SubscriptionModal component for upgrading to Pro

---
+++ client/app/components/SubscriptionModal.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Modal, Box, Typography, Button, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+const SubscriptionModal = ({ open, handleClose, handleGoPro }) => {
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 2,
+        }}
+      >
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Typography variant="h6" component="h2">
+          Upgrade to Pro
+        </Typography>
+        <Typography sx={{ mt: 2 }}>
+          To continue using Flashmaster, please upgrade to Pro.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleGoPro}
+          sx={{ mt: 3 }}
+        >
+          Go Pro
+        </Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export default SubscriptionModal;
